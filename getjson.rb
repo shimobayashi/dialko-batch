8310c54b0405d066c16a089025f1e0447d7906b4
shimobayashi <akimasa@shimobayashi.name>
2013-01-26 23:34:13 +0900

    カレントディレクトリのこと忘れてたので修正

--- getjson.rb
+++ getjson.rb
@@ -3,9 +3,10 @@ require 'json/pure'
 def getJson(yearago)
 
   filename = yearago.strftime('%Y_%m.js');
+  filename = File.dirname(__FILE__) + '/tweets/' + filename
 
   lines = []
-  File::open('tweets/' + filename) do |f|
+  File::open(filename) do |f|
     while l = f.gets
       lines << l
     end
